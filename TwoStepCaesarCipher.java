5e6929429f9498dd73ace5fadca8da2618a4e4bd
Sunila Prasad <rajeshwarrprasad@yahoo.com>
2017-09-01 16:04:24 +1200

    apply two levels of cipher

--- TwoStepCaesarCipher.java
+++ TwoStepCaesarCipher.java
@@ -9,25 +9,34 @@ public class TwoStepCaesarCipher {
         //Write down the alphabet
         String uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
         String alphabets = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
-        //Compute the shifted alphabet
+       
+        //Compute the shifted alphabet using key1
         String shiftedUppercase = uppercase.substring(key1)+
         uppercase.substring(0,key1);
         String ShiftedAlphabet1 = shiftedUppercase+ shiftedUppercase.toLowerCase();
        
+        // Compute the shifted alphabet using key2
+        shiftedUppercase = uppercase.substring(key2)+
+        uppercase.substring(0,key2);
+        String ShiftedAlphabet2 = shiftedUppercase+ shiftedUppercase.toLowerCase();
 
         //Count from 0 to < length of encrypted, (call it i)
         for(int i = 0; i < encrypted.length(); i++) {
             //Look at the ith character of encrypted (call it currChar)
             char currChar = encrypted.charAt(i);
-            System.out.print("Current char is "+ currChar);
-            
+            char newChar = (char) 0;
+
                 //Find the index of currChar in the alphabet (call it idx)
-                System.out.println(" it is uppercase ");
                 int idx = alphabets.indexOf(currChar);
                 //If currChar is in the alphabet
                 if(idx != -1){
-                    //Get the idxth character of shiftedUppercase (newChar)
-                    char newChar = ShiftedAlphabet1.charAt(idx);
+                    
+                if(i%2 == 0){
+                    newChar = ShiftedAlphabet1.charAt(idx);
+                }else{
+                    newChar = ShiftedAlphabet2.charAt(idx);                
+                }
+                    
                     //Replace the ith character of encrypted with newChar
                     encrypted.setCharAt(i, newChar);
                 }
@@ -39,10 +48,10 @@ public class TwoStepCaesarCipher {
         return encrypted.toString();
     }
     public void TestTwoStepCaesar() {
-        int key = 1;
+        int key = 0;
         FileResource fr = new FileResource();
         String message = fr.asString();
-        String encrypted = encrypt(message, key, key);
+        String encrypted = encrypt(message, key, 1);
         System.out.println(encrypted);
         //String decrypted = encrypt(encrypted, 26-key);
         //System.out.println(decrypted);
